82e2b2451d6b57dffdb6bb335fb54d988378ebc8
Eli Goldfine <manifed.markets@gmail.com>
2026-02-24 15:35:27 -0500

    Revert "Add landing portal and Phase‑1 governance workspace UI"

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import LandingPortal from "./pages/LandingPortal";
-import GovernanceWorkspace from "./pages/GovernanceWorkspace";
 import BeginnerMode from "./pages/BeginnerMode";
 import AdvancedMode from "./pages/AdvancedMode";
 import TeachingLab from "./pages/TeachingLab";
@@ -28,9 +26,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPortal />} />
-          <Route path="/studio" element={<Index />} />
-          <Route path="/governance-workspace" element={<GovernanceWorkspace />} />
+          <Route path="/" element={<Index />} />
           <Route path="/beginner" element={<BeginnerMode />} />
           <Route path="/advanced" element={<AdvancedMode />} />
           <Route path="/learn" element={<TeachingLab />} />
